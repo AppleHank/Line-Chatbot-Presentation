d46b7fb9d4d6c3d894d01453a64da68e0a76a451
AppleHank <a1053305@mail.nuk.edu.tw>
2021-05-19 15:29:56 +0800

    'image_message_test'

--- app_with_handler.py
+++ app_with_handler.py
@@ -26,6 +26,7 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
+from linebot.models.messages import ImageMessage
 
 app = Flask(__name__)
 
@@ -54,6 +55,14 @@ def callback():
 
     # handle webhook body
     try:
+        print("body:")
+        print(body)
+        print('-'*10)
+
+        print("signature")
+        print(signature)
+        print('-'*10)
+
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
@@ -63,9 +72,35 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
+    print("text event:")
+    print(event)
+    print('-'*10)
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text)
+    )
+
+@handler.add(MessageEvent, message=ImageMessage)
+def message_image(event):
+    print("image event:")
+    print(event)
+    print('-'*10)
+    
+    message_content = line_bot_api.get_message_content(event.message.id)
+    print("message_content:")
+    print(message_content)
+    print('-'*10)
+
+    file_path = 'sample_image/image_message.jpg'
+    with open(file_path, 'wb') as fd:
+        for chunk in message_content.iter_content():
+            fd.write(chunk)
+
+    response_image = open('sample_image/Kim.jpg','rb')
+    line_bot_api.reply_message(
+        event.relpy_token,
+        ImageMessage(response_image)
     )
 
 
